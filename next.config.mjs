64e6a3648a623b72bed8f05fbda0a788056a1cf6
elkins2k <elkins2k@mail.com>
2025-04-24 21:23:14 -0500

    feat: update serverActions configuration to allow specific origins

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ["*"]
+    },
   },
   transpilePackages: ['react-leaflet', '@react-leaflet/core'],
   webpack: (config, { isServer }) => {
